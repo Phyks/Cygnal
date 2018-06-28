302413598b2a371a7d99bced74238fbaf1305bbd
Phyks (Lucas Verney) <phyks@phyks.me>
2018-06-28 22:56:59 +0200

    Add link to source code in help

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -4,7 +4,7 @@ export default {
         availableReportsTitle: 'The available reports so far are:',
         gcumDescription: 'A car poorly parked on a bike lane. Such reports are automatically deleted after one hour, as they are by nature temporary.',
         interruptDescription: 'An interruption of the bike lane (works, unexpected end of the bike lane, etc.).',
-        license: 'It is released under an <a href="https://opensource.org/licenses/MIT">MIT license</a>. The map background is using tiles from <a href="https://www.opencyclemap.org/docs/">OpenCycleMap</a>, thanks to <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> and <a href="http://leafletjs.com/">Leaflet</a>.',
+        license: 'It is released under an <a href="https://opensource.org/licenses/MIT">MIT license</a> (<a href="https://framagit.org/phyks/cyclassist">source code</a>). The map background is using tiles from <a href="https://www.opencyclemap.org/docs/">OpenCycleMap</a>, thanks to <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> and <a href="http://leafletjs.com/">Leaflet</a>.',
         potholeDescription: 'A pothole in the ground.',
         summary: 'This app lets you track and share issues with bike lanes.',
         usage: 'How to use',
--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -4,7 +4,7 @@ export default {
         availableReportsTitle: "Les signalements disponibles pour l'instant sont :",
         gcumDescription: "Une voiture (mal) garée sur la piste cyclable. Ces signalements sont automatiquement supprimés au bout d'une heure car ils sont par essence temporaires.",
         interruptDescription: "Une interruption d'itinéraire cyclable (travaux, arrêt inattendu d'une piste cyclable, etc)",
-        license: "Le code source est sous <a href='https://opensource.org/licenses/MIT'>licence MIT license</a>. Les tuiles de fond de carte proviennent de chez <a href='https://www.opencyclemap.org/docs/'>OpenCycleMap</a>, grace aux <a href='https://www.openstreetmap.org/copyright'>contributeurs OpenStreetMap</a> et à <a href='http://leafletjs.com/'>Leaflet</a>.",
+        license: "Le code source est sous <a href='https://opensource.org/licenses/MIT'>licence MIT license</a> (<a href='https://framagit.org/phyks/cyclassist'>code source</a>). Les tuiles de fond de carte proviennent de chez <a href='https://www.opencyclemap.org/docs/'>OpenCycleMap</a>, grace aux <a href='https://www.openstreetmap.org/copyright'>contributeurs OpenStreetMap</a> et à <a href='http://leafletjs.com/'>Leaflet</a>.",
         potholeDescription: 'Un nid de poule dans la route.',
         summary: 'Cette application vous permet de signaler et de partager des problèmes avec les itinéraires cyclables.',
         usage: 'Utilisation',
