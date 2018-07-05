dde886d46eac75c53801e9639ef5d82e07925f5e
Phyks (Lucas Verney) <phyks@phyks.me>
2018-07-05 09:53:21 +0200

    Fix lint

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
 
 import * as actions from './actions';
 import { initialState as state, mutations } from './mutations';
 
 const plugins = [];
 if (process.NODE_ENV !== 'production') {
-    const createLogger = require('vuex/dist/logger');
     plugins.push(createLogger());
 }
 
