2f3232440942cf76744f448ed6bf107a2fb2272d
Phyks (Lucas Verney) <phyks@phyks.me>
2018-08-02 16:58:08 +0200

    Store complete GPX data
    Rework the store to keep track of everything useful for building a full
    GPX trace.

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,13 @@
 import { REPORT_VOTES_THRESHOLD } from '@/constants';
 
+export function getLastLocation(state) {
+    const gpx = state.location.gpx;
+    if (gpx.length > 0) {
+        return gpx[gpx.length - 1];
+    }
+    return null;
+}
+
 export function notDismissedReports(state) {
     return state.reports.filter((item) => {
         if (item.attributes.downvotes === 0) {
